3b9219366db8d78a19f9b1c194b3853d8efebea9
zb666 <1360799362@qq.com>
2018-11-03 14:13:32 +0800

    模块化基本框架搭建

---
+++ moudle_b/src/main/onmoudle/java/com/team/moudleb/OnMoudleBActivity.java
@@ -0,0 +1,17 @@
+package com.team.moudleb;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dongnao.module2.R;
+
+public class OnMoudleBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mooudleb);
+
+    }
+}
---
+++ moudle_b/src/main/onmoudle/java/com/team/moudleb/app/MoudleBApp.java
@@ -0,0 +1,12 @@
+package com.team.moudleb.app;
+
+import android.app.Application;
+
+public class MoudleBApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
