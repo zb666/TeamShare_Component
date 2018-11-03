9fc5c8d663325dc2b430f49574906b3dbef85af0
zb666 <1360799362@qq.com>
2018-11-03 15:00:48 +0800

    组件化搭建-》当单独成为一个app的时候，保存当前进度

--- app/src/main/java/com/example/mechrevo/teamshare_component/MainActivity.kt
+++ app/src/main/java/com/example/mechrevo/teamshare_component/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mechrevo.teamshare_component
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
 }
--- moudle_b/src/main/java/com/example/moudle_b/ActivityB.java
+++ moudle_b/src/main/java/com/example/moudle_b/ActivityB.java
@@ -1,5 +1,6 @@
-package com.team.moudleb;
+package com.example.moudle_b;
 
+import android.app.Application;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,15 +8,13 @@ import android.widget.TextView;
 
 import com.dongnao.module2.R;
 
-//成为一个单独的app的时候
-public class OnMoudleBActivity extends AppCompatActivity {
+public class ActivityB extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mooudleb);
-        TextView tvBBB = findViewById(R.id.tvOnMoudleB);
-        tvBBB.setText(getResources().getString(R.string.app_moudleb));
-
+        setContentView(R.layout.activity_b);
+        TextView textView = findViewById(R.id.tvB);
+        textView.setText("成为一个Moudle的时候");
     }
 }
